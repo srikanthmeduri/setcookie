cca9db0f62dc35dc94dae8300e090e5a59d19454
Srikanth Meduri <srikmed@gmail.com>
2015-03-29 00:44:56 +0530

    changes

--- app.js
+++ app.js
@@ -2,34 +2,42 @@ var express = require('express');
 var app = express();
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
     extended: true
 }));
-
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
+    res.writeHead(200, {
+        'content-type': 'text/html'
+    });
+    var q = req.query;
+    var s = '';
+    for (k in q) {
+        s += '<p>' + k + ' :: ' + q[k] + '</p>';
+    }
+    res.end(s);
+});
+/*
+app.get('/', function(req, res) {
+    console.log(req.query.id);
     if (req.cookies.remember) {
         res.send('Remembered :). Click to <a href="/forget">forget</a>!.');
     } else {
         res.send('<form method="post"><p>Check to <label>' + '<input type="checkbox" name="remember"/> remember me</label> ' + '<input type="submit" value="Submit"/>.</p></form>');
     }
 });
-
-app.get('/forget', function (req, res) {
+app.get('/forget', function(req, res) {
     res.clearCookie('remember');
     res.redirect('back');
 });
-
-app.post('/', function (req, res) {
+app.post('/', function(req, res) {
     var minute = 60 * 1000;
     if (req.body.remember) res.cookie('remember', 1, {
         maxAge: minute
     });
     res.redirect('back');
 });
-
-
+*/
 app.listen(3000);
 console.log('Server Started on port::: ', 3000);
